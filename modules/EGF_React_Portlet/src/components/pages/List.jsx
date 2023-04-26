2b73fee090c3d89333508d58c4f131c2fdd5d5e1
JorgeParreira <jorge.parreira@inetum.com>
2023-04-26 18:15:08 +0100

    Implementation of grouping menu. Still hard coded.

--- modules/EGF_React_Portlet/src/components/pages/List.jsx
+++ modules/EGF_React_Portlet/src/components/pages/List.jsx
@@ -4,8 +4,11 @@ import SearchBar from "../minorComponents/SearchBar";
 import Table from "../minorComponents/Table";
 import { data } from '../../data';
 
+
 export default function List() {
     const [query, setQuery] = useState("");
+    const [show, setShow] = useState("");
+    const [showSubCat, setShowSubCat] = useState("");
 
     // Define a função handleQuery que filtra a lista de documentos
     const handleQuery = (data) => {
@@ -18,6 +21,15 @@ export default function List() {
         )
     };
 
+    const handleToggle = (category) => {
+        show === category ? setShow("") : setShow(category);
+    };
+
+    const handleToggleSubCatgory = (subCat) => {
+        show === subCat ? setShowSubCat("") : setShowSubCat(subCat);
+    };
+
+
     // Define o listener que atualiza o estado 'query' sempre que o usuário digita na barra de pesquisa
     const searchBarListener = (e) => {
         setQuery(e.target.value);
@@ -26,27 +38,90 @@ export default function List() {
     return (
         <div className="content">
             <div className="go-back d-flex justify-content-end">
-                <Link className="btn btn-primary btn-outline-light btn-lg" role="button" to="/">Voltar</Link>
+                <Link className="btn btn-light" role="button" to="/">Voltar</Link>
             </div>
             <div className="page-title">
-                <h2>Documentos</h2>
+                <h2 className="mb-5">Documentos</h2>
             </div>
             <div className="container">
                 <div className="row m-3">
-                    <div className="col-3">
-                        <div className="dropdown">
-                            <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown link
-                            </a>
-
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </div>
+                    <div className="col-3 d-flex flex-column">
+                        <h6 className="btn btn-light d-flex" onClick={() => handleToggle("category")}>Categorias</h6>
+                        {
+                            show === "category" ?
+                                <div className="category">
+                                    <ul className="category-list" style={{ listStyleType: "none" }}>
+                                        <li><h6 className="btn btn-link"
+                                            style={{ textDecoration: "none" }}
+                                            onClick={() => handleToggleSubCatgory("category1")}> Categoria 1</h6>
+                                            {
+                                                showSubCat === "category1" ?
+                                                    <div className="category1">
+                                                        <ul className="doc-type-list" style={{ listStyleType: "none" }}>
+                                                            <li><input type="checkbox" /> Tipo 1</li>
+                                                            <li><input type="checkbox" /> Tipo 2</li>
+                                                            <li><input type="checkbox" /> Tipo 3</li>
+                                                        </ul>
+                                                    </div>
+                                                    : null
+                                            }
+                                        </li>
+                                        <li><h6 className="btn btn-link"
+                                            style={{ textDecoration: "none" }}
+                                            onClick={() => handleToggleSubCatgory("category2")}> Categoria 2</h6>
+                                            {
+                                                showSubCat === "category2" ?
+                                                    <div className="category2">
+                                                        <ul className="doc-type-list" style={{ listStyleType: "none" }}>
+                                                            <li><input type="checkbox" /> Tipo 1</li>
+                                                            <li><input type="checkbox" /> Tipo 2</li>
+                                                            <li><input type="checkbox" /> Tipo 3</li>
+                                                        </ul>
+                                                    </div>
+                                                    : null
+                                            }</li>
+                                        <li><h6 className="btn btn-link"
+                                            style={{ textDecoration: "none" }}
+                                            onClick={() => handleToggleSubCatgory("category3")}> Categoria 3</h6>
+                                            {
+                                                showSubCat === "category3" ?
+                                                    <div className="category3">
+                                                        <ul className="doc-type-list" style={{ listStyleType: "none" }}>
+                                                            <li><input type="checkbox" /> Tipo 1</li>
+                                                            <li><input type="checkbox" /> Tipo 2</li>
+                                                            <li><input type="checkbox" /> Tipo 3</li>
+                                                        </ul>
+                                                    </div>
+                                                    : null
+                                            }</li>
+                                    </ul>
+                                </div>
+                                : null
+                        }
+                        <h6 type="button" className="btn btn-light d-flex" onClick={() => handleToggle("company")}>Empresas</h6>
+                        {
+                            show === "company" ?
+                                <div className="company">
+                                    <ul className="company-list" style={{ listStyleType: "none" }}>
+                                        <li><input type="checkbox" /> Empresa 1</li>
+                                        <li><input type="checkbox" /> Empresa 2</li>
+                                        <li><input type="checkbox" /> Empresa 3</li>
+                                    </ul>
+                                </div>
+                                : null
+                        }
+                        <h6 type="button" className="btn btn-light d-flex" onClick={() => handleToggle("date")}>Data</h6>
+                        {
+                            show === "date" ?
+                                <div className="date">
+                                    <ul className="date-list" style={{ listStyleType: "none" }}>
+                                        <li> Depois de <input type="date" /></li>
+                                        <li> Antes de   <input type="date" /></li>  
+                                    </ul>
+                                </div>
+                                : null
+                        }
                     </div>
-
                     <div className="col-9">
                         <div className="search">
                             <SearchBar placeholder="Pesquise documentos" onChange={searchBarListener} />
